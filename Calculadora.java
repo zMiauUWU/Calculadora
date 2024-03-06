8a632e9d82a686313ed1c79f7bd2f441a1495b1f
Rafael <141313522+zMiauUWU@users.noreply.github.com>
2024-03-05 21:35:32 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,62 @@
+package calculadora;
+
+public class Calculadora {
+
+    private float resultado;
+    private char op;
+    private int[] array;
+
+    Calculadora(int[] array, char op) {
+        this.array = array;
+        this.op = op;
+        if (this.op == '+') {
+            System.out.println("A operação é soma!");
+            Soma();
+
+        } else if (this.op == '-') {
+            System.out.println("A operação é subtração!");
+            Subtração();
+
+        } else if (this.op == '*') {
+            System.out.println("A operação é multiplicação!");
+            Multiplicação();
+        } else if (this.op == '/') {
+            System.out.println("A operação é Divisão");
+            Divisão();
+        }
+
+        System.out.println(this.op);
+
+    }
+
+    private void Soma() {
+        for (int i = 0; i < this.array.length; i++) {
+            this.resultado += this.array[i];
+        }
+    }
+
+    private void Subtração() {
+        for (int i = 0; i < this.array.length; i++) {
+            this.resultado -= this.array[i];
+        }
+    }
+
+    private void Multiplicação() {
+        this.resultado = 1;
+        for (int i = 0; i < this.array.length; i++) {
+            this.resultado *= this.array[i];
+        }
+    }
+
+    private void Divisão() {
+        this.resultado = 1;
+        for (int i = 0; i < this.array.length; i++) {
+            this.resultado /= this.array[i];
+        }
+    }
+
+    public Float GetResultado() {
+        return this.resultado;
+    }
+
+}
